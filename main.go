a028be2acee8d63ea6d5079ccfa90a0717361212
Jonathan Duck <duckbrain30@gmail.com>
2017-01-18 08:37:15 -0800

    Opens vim to the correct date

--- main.go
+++ main.go
@@ -2,28 +2,45 @@ package main
 
 import "fmt"
 import "os"
+import "os/exec"
+import "time"
 import "strings"
 import "github.com/olebedev/when"
 import "github.com/olebedev/when/rules/en"
 import "github.com/olebedev/when/rules/common"
 
 func main() {
-	w := when.New(nil)
-	w.Add(en.All...)
-	w.Add(common.All...)
-
 	if len(os.Args) < 2 {
 		fmt.Println("You must have a folder name")
+		return
 	}
-	folderName := os.Args[1]
-	var dateString string
+	folder := os.Args[1]
+	var date time.Time
 
 	if len(os.Args) > 2 {
-		dateString = strings.Join(os.Args[2:], " ")
+		w := when.New(nil)
+		w.Add(en.All...)
+		w.Add(common.All...)
+		dateString := strings.Join(os.Args[2:], " ")
+		result, err := w.Parse(dateString, time.Now())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		date = result.Time
 	} else {
-		dateString = "today"
+		date = time.Now()
 	}
 
-	fmt.Printf("Folder: %v, Date: %v\n", folderName, dateString)
+	dateString := date.Format("2006-01-02")
+	fileName := fmt.Sprintf("~/Documents/%v/%v-%v.md", folder, folder, dateString)
 
+	cmd := exec.Command("vim", fileName)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
